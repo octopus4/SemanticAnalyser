7beda2861952a74494b81694f3e7d3e079a28197
octopus4 <laokrit@gmail.com>
2018-04-05 16:56:15 +0300

    System.Windows.Media.Color adapter was created

--- SomVisualisation/ColorAdapter.cs
+++ SomVisualisation/ColorAdapter.cs
@@ -1,4 +1,4 @@
-﻿namespace SomVisualisation
+﻿namespace Visualisation
 {
     public class ColorAdapter
     {
@@ -27,6 +27,45 @@
 
         public ColorAdapter(int a, int r, int g, int b) : this((byte)a, (byte)b, (byte)g, (byte)b)
         {
+        }
+
+        public static bool operator == (ColorAdapter left, ColorAdapter right)
+        {
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+            if (!ReferenceEquals(left, null) || !ReferenceEquals(right, null))
+            {
+                return false;
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ColorAdapter left, ColorAdapter right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var adapter = obj as ColorAdapter;
+            return adapter != null &&
+                A == adapter.A &&
+                R == adapter.R &&
+                G == adapter.G &&
+                B == adapter.B;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1749689076;
+            hashCode = hashCode * -1521134295 + A.GetHashCode();
+            hashCode = hashCode * -1521134295 + R.GetHashCode();
+            hashCode = hashCode * -1521134295 + G.GetHashCode();
+            hashCode = hashCode * -1521134295 + B.GetHashCode();
+            return hashCode;
         }
     }
 }
