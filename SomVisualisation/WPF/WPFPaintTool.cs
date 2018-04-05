f6351428e8dc5fa6983e9dd67775d46821f3b51e
octopus4 <laokrit@gmail.com>
2018-04-05 17:00:27 +0300

    Graph displaying functionality was added

--- SomVisualisation/WPF/WPFPaintTool.cs
+++ SomVisualisation/WPF/WPFPaintTool.cs
@@ -1,8 +1,9 @@
 ﻿using System.Globalization;
 using System.Windows;
 using System.Windows.Media;
+using Visualisation.Graph;
 
-namespace SomVisualisation.WPF
+namespace Visualisation.WPF
 {
     internal class WPFPaintTool : PaintTool
     {
@@ -29,10 +30,12 @@ namespace SomVisualisation.WPF
 
         internal override void DrawText(string text, float x, float y)
         {
+            byte alpha = Opacity.HasValue ? (byte)(Opacity.Value) : (byte)255;
+            Brush brush = new SolidColorBrush(Color.FromArgb(alpha, 0, 0, 0));
             CultureInfo culture = CultureInfo.CurrentCulture;
             FlowDirection flow = FlowDirection.LeftToRight;
             Typeface typeface = new Typeface(new FontFamily("Segoe UI"), FontStyles.Normal, FontWeights.SemiBold, FontStretches.SemiCondensed);
-            Context.DrawText(new FormattedText(text, culture, flow, typeface, FontSize, Brushes.Black), new Point(x, y));
+            Context.DrawText(new FormattedText(text, culture, flow, typeface, FontSize, brush), new Point(x, y));
         }
 
         internal override void SetColor(ColorAdapter color)
@@ -51,6 +54,23 @@ namespace SomVisualisation.WPF
         public override void Dispose()
         {
             Context.Close();
+        }
+
+        internal override void DrawNode(float x, float y, Node node)
+        {
+            byte alpha = Opacity.HasValue ? (byte)Opacity : node.Color.A;
+            Color brushColor = Color.FromArgb(alpha, node.Color.R, node.Color.G, node.Color.B);
+            Brush brush = new SolidColorBrush(brushColor);
+            Context.DrawEllipse(brush, new Pen(brush, 2), new Point(x, y), Node.Radius, Node.Radius);
+            DrawText(node.Word, x + 2, y - 2);
+        }
+
+        internal override void DrawLine(float x1, float y1, float x2, float y2, ColorAdapter color)
+        {
+            byte alpha = Opacity.HasValue ? (byte)Opacity : color.A;
+            Color brushColor = Color.FromArgb(alpha, color.R, color.G, color.B);
+            Brush brush = new SolidColorBrush(brushColor);
+            Context.DrawLine(new Pen(brush, 2), new Point(x1, y1), new Point(x2, y2));
         }
     }
 }
