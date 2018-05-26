96d4bea9c69bed3d455e781147828ed4fd437718
octopus4 <laokrit@gmail.com>
2018-05-26 04:31:42 +0300

    MapPresenter was simplified
    Part of MapPresenter functionality was moved to another classes, so the base class was greatly simplified.

--- SomVisualisation/SOM/MapPresenter.cs
+++ SomVisualisation/SOM/MapPresenter.cs
@@ -1,49 +1,53 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Visualisation.Drawers;
-using Visualisation.Drawers.Numerical;
-using Visualisation.Drawers.Categorial;
-using Visualisation.Drawers.Semantic;
 using DataPreprocessing.Data;
 using DataPreprocessing.Data.Semantic;
+using Visualisation.SOM.Drawers;
+using Visualisation.SOM.Drawers.Numerical;
+using Visualisation.SOM.Drawers.Categorial;
+using Visualisation.SOM.Drawers.Semantic;
 using SOM;
+using System;
 
-namespace Visualisation
+namespace Visualisation.SOM
 {
     public class MapPresenter : DataPresenter
     {
-        private int NeuronWidth { get; set; }
-        private int NeuronHeight { get; set; }
-
+        private double MapWidth { get; set; }
+        private double MapHeight { get; set; }
+        private double NeuronWidth { get; set; }
+        private double NeuronHeight { get; set; }
+        private List<Cluster> Clusters { get; set; }
         private Dictionary<string, Canvas> Maps { get; set; }
 
         public int Index { get; set; }
         public string[] Headers { get; private set; }
-
         public List<string> SelectedNeuronRecords { get; private set; }
 
         public MapPresenter(int width, int height, DataSource source, CanvasCreator componentCreator, string[] headers, IView view)
             : base(width, height, source, componentCreator, view)
         {
+            CaptureArea = new Rectangle(0, 0, 1, 1);
             Headers = headers;
         }
 
         public override void Init(Dictionary<Neuron, List<DataToken>> clusterResults)
         {
             ClusterResults = clusterResults;
+            Clusters = clusterResults.Select(pair => new Cluster(pair.Key, pair.Value)).ToList();
 
-            SetCellSize(ClusterResults.Keys);
+            MapWidth = ClusterResults.Keys.Select(neuron => neuron.Position.X).Max() + 1;
+            MapHeight = ClusterResults.Keys.Select(neuron => neuron.Position.Y).Max() + 1;
+
+            SetCellSize();
             DrawMaps();
             Invalidate();
         }
 
-        private void SetCellSize(IEnumerable<Neuron> neurons)
+        private void SetCellSize()
         {
-            double mapWidth = neurons.Select(neuron => neuron.Position.X).Max() + 1;
-            double mapHeight = neurons.Select(neuron => neuron.Position.Y).Max() + 1;
-
-            NeuronWidth = (int)(Width / mapWidth);
-            NeuronHeight = (int)(Height / mapHeight);
+            NeuronWidth = ToScreen(1 / MapWidth, Width);
+            NeuronHeight = ToScreen(1 / MapHeight, Height);
         }
 
         private void DrawMaps()
@@ -51,45 +55,60 @@ namespace Visualisation
             Maps = new Dictionary<string, Canvas>();
             for (int i = 0; i < Source.DataTypes.Length; i++)
             {
-                Maps.Add(Headers[i], DrawMap(i, Source.DataTypes[i]));
+                Maps.Add(Headers[i], DrawMap(i));
             }
         }
 
-        private Canvas DrawMap(int index, DataType dataType)
+        private Canvas DrawMap(int index)
         {
+            Action<PaintTool, Cluster> action = GetDrawingAction(index, Source.DataTypes[index]);
+            Canvas canvas = ComponentCreator.CreateCanvas(Width, Height);
+            PaintTool tool = canvas.Tool;
+            tool.StartRendering();
+            foreach (Cluster cluster in Clusters)
+            {
+                cluster.ToRelative(CaptureArea);
+                action(tool, cluster);
+            }
+            tool.Dispose();
+
+            return canvas;
+        }
+
+        private Action<PaintTool, Cluster> GetDrawingAction(int index, DataType dataType)
+        {
+            SetCellSize();
             switch (dataType)
             {
                 case DataType.Numerical:
                     var numericalData = SourceTokens.Select(token => double.Parse(token.Values[index].ToString()));
-                    MapDrawer<double> numericalDrawer = new NumericalMapDrawer(ClusterResults, index, NeuronWidth, NeuronHeight, ComponentCreator, numericalData);
-                    return numericalDrawer.Draw(Width, Height);
+                    MapDrawer<double> numericalDrawer = new NumericalMapDrawer(ClusterResults, index, NeuronWidth, NeuronHeight, numericalData);
+                    return numericalDrawer.Draw;
 
                 case DataType.Categorial:
                     var categorialData = SourceTokens.Select(token => token.Values[index].ToString());
-                    MapDrawer<string> categorialDrawer = new CategorialMapDrawer(ClusterResults, index, NeuronWidth, NeuronHeight, ComponentCreator, categorialData);
-                    return categorialDrawer.Draw(Width, Height);
+                    MapDrawer<string> categorialDrawer = new CategorialMapDrawer(ClusterResults, index, NeuronWidth, NeuronHeight, categorialData);
+                    return categorialDrawer.Draw;
 
                 case DataType.Semantic:
-                    MapDrawer<SemanticPair[]> semanticDrawer = new SemanticMapDrawer(ClusterResults, index, NeuronWidth, NeuronHeight, ComponentCreator);
-                    return semanticDrawer.Draw(Width, Height);
+                    MapDrawer<SemanticPair[]> semanticDrawer = new SemanticMapDrawer(ClusterResults, index, NeuronWidth, NeuronHeight);
+                    return semanticDrawer.Draw;
+
                 default:
-                    return null;
+                    throw new ArgumentException("Provided data type is not valid", nameof(dataType));
             }
         }
 
         protected override void MouseClick(double x, double y)
         {
-            double i = x / NeuronWidth;
-            double j = y / NeuronHeight;
-
-            Neuron neuron = ClusterResults.Keys.First(n => n.Position.X == i && n.Position.Y == j);
-            SelectedNeuronRecords = ClusterResults[neuron].Select(token => token.Values[Index].ToString()).ToList();
+            return;
         }
 
         protected override void OnInvalidate()
         {
             List<string> keys = Maps.Keys.ToList();
             string attribute = keys[Index];
+            Maps[attribute] = DrawMap(Index);
             Image = Maps[attribute];
         }
     }
